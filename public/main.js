35a2ca4817d952361e61b0ce2b44edfb6f472b19
Jay Lhomme <jhgatohagire@gmail.com>
2023-11-20 19:22:42 -0600

    place holder inits

--- public/main.js
+++ public/main.js
@@ -3,20 +3,72 @@
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
+const jokeEndpoint = 'https://v2.jokeapi.dev/joke/Programming?safe-mode&type=twopart';
+const setup = [];
+const punchline = [];
+
+const getRequest = new Promise((resolve, reject) => {
+  fetch(jokeEndpoint, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+console.warn(getRequest);
+
 const init = () => {
   document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
+    <h1>Welcome! to Joke-Generator</h1>
+    <button class="btn btn-danger" id="getJoke">Get a Joke!</button><br />
     <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
   `;
   console.warn('YOU ARE UP AND RUNNING!');
 
+  const init2 = () => {
+    document.querySelector('#app').innerHTML = `
+      <h1>Welcome! to Joke-Generator</h1>
+      <button class="btn btn-danger" id="getPunchline">Get Punchline!</button><br />
+      <hr />
+      <h3>${setup}</h3>
+    `;
+  };
+
+  const init3 = () => {
+    document.querySelector('#app').innerHTML = `
+      <h1>Welcome! to Joke-Generator</h1>
+      <button class="btn btn-danger" id="getAnotherJoke">Get Another Joke!</button><br />
+      <hr />
+      <h3>${setup}</h3>
+      <h3>${punchline}</h3>
+    `;
+  };
+
   document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
+    .querySelector('#getJoke')
+    .addEventListener('click', () => {
+      setup.push(getRequest.then());
+      return init2();
+    });
+
+  document
+    .querySelector('#getPunchline')
+    .addEventListener('click', () => {
+      punchline.push(getRequest.then());
+      return init3();
+    });
+
+  document
+    .querySelector('#getAnotherJoke')
+    .addEventListener('click', () => {
+      setup.push(getRequest.then());
+      return init2();
+    });
+  // () => console.warn('You clicked that button!')
 
   // USE WITH FIREBASE AUTH
   // ViewDirectorBasedOnUserAuthStatus();
